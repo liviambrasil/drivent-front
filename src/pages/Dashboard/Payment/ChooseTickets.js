3d8042c6c9a4dde1ca644ed1066b2f654c5ea737
Lívia Brasil <liviabrasil@id.uff.br>
2021-09-02 19:53:41 -0300

    fix: correct global font

--- src/pages/Dashboard/Payment/ChooseTickets.js
+++ src/pages/Dashboard/Payment/ChooseTickets.js
@@ -1,11 +1,27 @@
+import { Typography } from "@material-ui/core";
 import {
   useHistory,
 } from "react-router-dom";
 import styled from "styled-components";
+import { Title } from "../../../components/Auth";
 
 export default function ChooseTickets( { match } ) {
   const history=useHistory();
   return(
-    <button onClick={() => history.push(`${match.path}/complete`)}> "exemplo parte da livia"</button>
+    <>
+      <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
+      <Text>Primeiro, escolha sua modalidade de ingresso</Text>
+      <button onClick={() => history.push(`${match.path}/complete`)}> "exemplo parte da livia"</button>
+    </>
   );
 }
+
+const Text = styled.p`
+font-size: 20px;
+color: #8E8E8E;
+`;
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
+
